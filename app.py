701f965b805b1150f5a98092d769dc078bb6f3f5
mduhagon <mduhagon@gmail.com>
2022-06-18 19:35:39 +0200

    Procfile for heroku

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def create_app(test_config=None):
     SECRET_KEY = os.urandom(32)
     app.config['SECRET_KEY'] = SECRET_KEY
 
-    """ uncomment at the first time running the app """
-    # db_drop_and_create_all()
+    """ uncomment at the first time running the app. Then comment back so you do not erase db content over and over """
+    db_drop_and_create_all()
 
     @app.route('/', methods=['GET'])
     def home():
